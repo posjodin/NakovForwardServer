753388ad50c43f08ebfccc178c217ecbf30ef81f
Peter Sjödin <peter@sjodin.net>
2018-12-02 23:55:29 +0100

    Client side forwarding process

--- ForwardServerClientThread.java
+++ ForwardServerClientThread.java
@@ -13,23 +13,26 @@ import java.io.OutputStream;
  
 public class ForwardServerClientThread extends Thread
 {
-    private NakovForwardServer mNakovForwardServer = null;
-    private NakovForwardServer.ServerDescription mServer = null;
+    private ForwardClient mForwardClient = null;
     private Socket mClientSocket = null;
     private Socket mServerSocket = null;
     private boolean mBothConnectionsAreAlive = false;
     private String mClientHostPort;
     private String mServerHostPort;
- 
+    private int mServerPort;
+    private String mServerHost;
     /**
      * Creates a client thread for handling clients of NakovForwardServer.
      * A client socket should be connected and passed to this constructor.
      * A server socket is created later by run() method.
      */
-    public ForwardServerClientThread(NakovForwardServer aNakovForwardServer, Socket aClientSocket)
+    public ForwardServerClientThread(ForwardClient ForwardClient, Socket aClientSocket, String serverhost, int serverport)
     {
-        mNakovForwardServer = aNakovForwardServer;
+        mForwardClient = ForwardClient;
         mClientSocket = aClientSocket;
+        mServerPort = serverport;
+        mServerHost = serverhost;
+
     }
  
     /**
@@ -43,14 +46,7 @@ public class ForwardServerClientThread extends Thread
         try {
            mClientHostPort = mClientSocket.getInetAddress().getHostAddress() + ":" + mClientSocket.getPort();
  
-           // Create a new socket connection to one of the servers from the list
-           mServerSocket = createServerSocket();
-           if (mServerSocket == null) {  // If all the servers are down
-               System.out.println("Can not establish connection for client " +
-                   mClientHostPort + ". All the servers are down.");
-               try { mClientSocket.close(); } catch (IOException e) {}
-               return;
-           }
+           mServerSocket = new Socket(mServerHost, mServerPort);
  
            // Obtain input and output streams of server and client
            InputStream clientIn = mClientSocket.getInputStream();
@@ -58,8 +54,8 @@ public class ForwardServerClientThread extends Thread
            InputStream serverIn = mServerSocket.getInputStream();
            OutputStream serverOut = mServerSocket.getOutputStream();
  
-           mServerHostPort = mServer.host + ":" + mServer.port;
-           mNakovForwardServer.log("TCP Forwarding  " + mClientHostPort + " <--> " + mServerHostPort + "  started.");
+           mServerHostPort = mServerHost + ":" + mServerPort;
+           mForwardClient.log("TCP Forwarding  " + mServerPort + " <--> " + mServerHostPort + "  started.");
  
            // Start forwarding of socket data between server and client
            ForwardThread clientForward = new ForwardThread(this, clientIn, serverOut);
@@ -89,57 +85,9 @@ public class ForwardServerClientThread extends Thread
            try { mClientSocket.close(); } catch (IOException e) {}
  
            mBothConnectionsAreAlive = false;
-           mServer.clientsConectedCount--;
  
-           mNakovForwardServer.log("TCP Forwarding  " + mClientHostPort + " <--> " + mServerHostPort + "  stopped.");
+           mForwardClient.log("TCP Forwarding  " + mClientHostPort + " <--> " + mServerHostPort + "  stopped.");
         }
-    }
- 
-    /**
-     * @return a new socket connected to some of the servers in the destination
-     * servers list. Sequentially a connection to the least loaded server from
-     * the list is tried to be established. If connecting to some alive server
-     * fail, this server it marked as dead and next alive server is tried. If all
-     * the servers are dead, null is returned. Thus if at least one server is alive,
-     * a connection will be established (of course after some delay) and the system
-     * will not fail (it is fault tolerant). Dead servers can be marked as alive if
-     * revived, but this is done later by check alive thread.
-     */
-    private Socket createServerSocket() throws IOException
-    {
-        while (true) {
-           mServer = getServerWithMinimalLoad();
-           if (mServer == null)  // All the servers are down
-               return null;
-           try {
-               Socket socket = new Socket(mServer.host, mServer.port);
-               mServer.clientsConectedCount++;
-               return socket;
-           } catch (IOException ioe) {
-               mServer.isAlive = false;
-           }
-        }
-    }
- 
-    /**
-     * @return the least loaded alive server from the server list if load balancing
-     * is enabled or first alive server from the list if load balancing algorithm is
-     * disabled or null if all the servers in the list are dead.
-     */
-    private NakovForwardServer.ServerDescription getServerWithMinimalLoad()
-    {
-        NakovForwardServer.ServerDescription minLoadServer = null;
-        NakovForwardServer.ServerDescription[] servers = mNakovForwardServer.getServersList();
-        for (int i=0; i<servers.length; i++) {
-           if (servers[i].isAlive) {
-               if ((minLoadServer==null) || (servers[i].clientsConectedCount < minLoadServer.clientsConectedCount))
-                   minLoadServer = servers[i];
-               // If load balancing is disabled, return first alive server
-               if (!mNakovForwardServer.isLoadBalancingEnabled())
-                   break;
-            }
-        }
-        return minLoadServer;
     }
  
 }
